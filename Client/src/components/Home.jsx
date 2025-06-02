a95e45ebbaa07df7680403911d326ee44863fcad
Fatima Nur <fatimanur424@example.com>
2025-06-03 02:04:31 +0500

    Properly re-add client folder files

---
+++ Client/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/Home.css'
+const Home = () => {
+  return (
+    <div className='hero'>
+        <div className='hero-content'>
+            <h1 className='hero-text'>Welcome to the Book Store</h1>
+            <p className='hero-description'>Discover your next great read. Browse the collection of our best top interresting Books. You will def find what you are looking for!</p>
+        </div>
+        <div className='hero-image'>
+        </div>
+    </div> 
+  )
+}
+
+export default Home
